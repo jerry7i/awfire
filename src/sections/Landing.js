7ecbdd5fbb51a66241d0436a6f766befe4c1c50e
Jerry Li <jerry7i@hotmail.com>
2020-08-29 18:02:11 -0400

    complete mock responsive showcase

--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -1,21 +1,50 @@
 import React from "react";
 import styled from "styled-components";
+import { device } from "../device";
+import showcaseBg from "../images/fire_sprinkler_bg.jpg";
 
-const LandingWrapper = styled.section`
-	display: flex;
-	align-items: center;
+const LandingContainer = styled.div`
 	margin-bottom: 1000px;
-`
-const Title = styled.h2`
-	color: #000;
+	display: block;
+`;
+
+const Showcase = styled.section`
+	background-image: url(${showcaseBg});
+	background-size: cover;
+	background-position: 40% 0;
+	width: 100%;
+	margin-bottom: 30px;	
+
+	@media ${device.mobileS} {
+		height: 400px;
+		padding-top: 100px;
+	}
+
+	@media ${device.mobileL} {
+		height: 400px;
+		padding-top: 150px;
+	}
+
+	@media ${device.tablet} {
+		height: 700px;
+		padding-top: 250px;
+	}
+`;
+
+const ShowcaseText = styled.h2`
+	color: #fff;
+	text-shadow: 2.5px 2.5px 0 #ff6347;
+	margin: 0px 30px;
 `;
 
 const Landing = () => (
-	<LandingWrapper>
-		<Title>
-			Landing
-		</Title>
-	</LandingWrapper>
+	<LandingContainer>
+		<Showcase>
+			<ShowcaseText>
+				Design, Inspect, Test, Troubleshoot & Maintain:
+			</ShowcaseText>
+		</Showcase>
+	</LandingContainer>
 );
 
 
